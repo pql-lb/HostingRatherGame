dc3b57037684e3f3dec29e91c6394cd9600f94ec
lbaker15 <laelbaker@hotmail.co.uk>
2021-01-29 14:22:16 +0000

    Updates

---
+++ precache-manifest.8d5232b3de3cc50be79dc7cf14e06194.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9411c6e915cc08138efd9918e5161b5f",
+    "url": "/HostingRatherGame/index.html"
+  },
+  {
+    "revision": "3fc0dab42aac54593ea1",
+    "url": "/HostingRatherGame/static/css/main.7bf7e341.chunk.css"
+  },
+  {
+    "revision": "32ac22749f64662eb5da",
+    "url": "/HostingRatherGame/static/js/2.90bf943a.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/HostingRatherGame/static/js/2.90bf943a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc0dab42aac54593ea1",
+    "url": "/HostingRatherGame/static/js/main.9d58e118.chunk.js"
+  },
+  {
+    "revision": "db43d52fd85420451a8f",
+    "url": "/HostingRatherGame/static/js/runtime-main.36bf4983.js"
+  },
+  {
+    "revision": "548a659c2b06a877516d3c998f5b0939",
+    "url": "/HostingRatherGame/static/media/dog.548a659c.png"
+  },
+  {
+    "revision": "2eaee86e698e4323e74545312c78bfd5",
+    "url": "/HostingRatherGame/static/media/elephant.2eaee86e.png"
+  },
+  {
+    "revision": "68a5aa104457ecac4d4136285a830e3e",
+    "url": "/HostingRatherGame/static/media/monkey.68a5aa10.png"
+  }
+]);
